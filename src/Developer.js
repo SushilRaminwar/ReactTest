7a49e32a5b3dc5657692016782005c4add6f6b1e
sushil raminwar <sushilraminwar@gmail.com>
2020-04-04 02:41:23 +0530

    inheritance in Developer.js

--- src/Developer.js
+++ src/Developer.js
@@ -10,5 +10,15 @@ class Developer {
   
   var nathan = new Developer('Nathan');
 nathan.hello(); // Hello World! I am Nathan and I am a web developer
-console.log('asd');
+console.log('%casd asd', 'color: blue');
 console.log(nathan.hello());
+
+class ReactDeveloper extends Developer {
+  installReact(){
+    return 'installing React .. Done.';
+  }
+}
+
+var nathanasd = new ReactDeveloper('Nathan');
+console.log(nathanasd.hello()); // Hello World! I am Nathan and I am a web developer
+console.log(nathanasd.installReact()); // installing React .. Done.
